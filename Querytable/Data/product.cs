652a54a659ab2d60037c9a4f522a1d1f7185ceee
DGR0US <afbat4661@outlook.com>
2022-10-06 16:02:42 +0700

    Add ProductTable

---
+++ Querytable/Data/product.cs
@@ -0,0 +1,12 @@
+﻿namespace Querytable.Data
+{
+    public class product
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string name1 { get; set; }
+        public string name2 { get; set; }
+        public float price { get; set; }
+
+    }
+}
---
+++ Querytable/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Npgsql;
+using Querytable.Data;
+
+namespace Querytable.Services
+{
+    public interface IProductService
+    {
+        List<product> GetAllProduct();
+    }
+    public class ProductService : IProductService
+    {
+        public List<product> GetAllProduct()
+        {
+            List<product> Res = new List<product>();
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(GbVer.Connectionstring))
+                {
+                    con.Open();
+                    string SQL = "SELECT * FROM product ORDER BY code";
+                    Res = con.Query<product>(SQL).ToList();
+                }
+            }
+            catch (Exception ex)
+            {}
+            return Res;
+        }
+
+    }
+}
